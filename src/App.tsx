38acc261242bafae2f4873ce6ac4b3332b7b352a
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-12-03 16:06:50 -0300

    feat : initial files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import DashBoard from "./components/dashboard/dashboard"
+import Navbar from "./components/header/navbar"
+
+function App() {
+
+  return (
+    <main>
+      <Navbar/>
+      <DashBoard/>
+    </main>
+  )
+}
+
+export default App
