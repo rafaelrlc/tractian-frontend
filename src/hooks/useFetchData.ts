484b5996eb847a2dd4be9abed9772eec88a45cf5
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-12-08 22:05:47 -0300

    feat : add custom hook for fetching data based on selected company

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,19 @@
+import api from "../services/api";
+import { Company } from "../store/types";
+
+const useFetchData = (company: Company | undefined) => {
+  const fetchData = async (endpoint: string, errorMessage: string): Promise<any[]> => {
+    if (!company?.id) return [];
+    try {
+      const response = await api.get(`/${company.id}/${endpoint}`);
+      return response.data;
+    } catch {
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+  };
+
+  return { fetchData };
+};
+
+export default useFetchData;
